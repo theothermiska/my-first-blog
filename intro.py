195ea9964ea4a0d83afa4c6466cfdeb13988ed77
Michaela <meeshah.monkey@gmail.com>
2017-06-11 14:30:12 +0200

    My Django Girls app, first commit

---
+++ intro.py
@@ -0,0 +1,27 @@
+# print("ahoj")
+# teplo = input("je dneska teplo?")
+# if teplo == "ano":
+  #  print("nezapomeň sluneční brýle")
+# else:
+  #  print("nezapomeň bundu")
+
+# číslo = int(input("vlož číslo"))
+# if číslo > 10:
+  #  print("větší než 10")
+# elif číslo > 20:
+  #  print("větší než 20")
+# else:
+  #  print("menší než 10")
+
+def hi(name):
+    print("Hi " + name + " !")
+
+girls = ["Petra", "Monika", "Jana", "You"]
+for name in girls:
+    hi(name)
+    print("další")
+
+
+name = input("jak se jmenuješ?")
+def hii(name):
+    print("ahoj " + name)
